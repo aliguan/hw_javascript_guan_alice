ce00efcff7ac28c7f57c20812bf09b03c20f6fbb
Alice Guan <aliguan726@gmail.com>
2016-02-04 14:58:16 -0500

    Add presidents array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 var iceCream = [ 'Pistachio', 'Butter Pecan', 'Dulce de Leche' ];
 
 iceCream.push( 'Cookie Dough' );
+
+var presidents = [
+    { firstName: 'Barack', lastName: 'Obama', termLength: '2', party: 'Democrat', yearsOfPresidency: '8' },
+    { firstName: 'George W.', lastName: 'Bush', termLength: '2', party: 'Republican', yearsOfPresidency: '8' },
+    { firstName: 'Bill', lastName: 'Clinton', termLength: '2', party: 'Democrat', yearsOfPresidency: '8' },
+    { firstName: 'George H.', lastName: 'Bush', termLength: '1', party: 'Republican', yearsOfPresidency: '4' },
+    { firstName: 'Ronald', lastName: 'Reagan', termLength: '2', party: 'Republican', yearsOfPresidency: '8' }
+];
+
+console.log( presidents[ 2 ].firstName + ' ' + presidents[ 2 ].lastName );
